990c45d43d526733b5bd36cd3fa0be0ab928c6fd
kballou <kballou@devnulllabs.io>
2016-06-21 11:56:28 -0600

    Version bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Toglx.Mixfile do
     [app: :toglx,
      description: description,
      package: package,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
